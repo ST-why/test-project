58a3e14093a1dbc45219d7fdcfdd0b5810239706
taeco <styup1004@gmail.com>
2024-11-05 16:36:37 +0900

    newpatch

--- testproject.java
+++ testproject.java
@@ -1,6 +1,6 @@
 public class testproject{
     public static void main(String[] args) {
         System.out.println("새로운 프로젝트 시작이다");
-        System.out.println("새로운 버전 업데이트 중...");
+        System.out.println("브랜치에서 테스트 중.");
     }
 }
